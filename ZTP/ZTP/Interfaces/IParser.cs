40c94c8a69eb9e4b6cfbf72a55bfc243ffa7ba15
MikoloD <grygiel.mikolaj@gmail.com>
2022-03-08 22:40:29 +0100

    Create IParser.cs

---
+++ ZTP/ZTP/Interfaces/IParser.cs
@@ -0,0 +1,12 @@
+﻿using Graphviz4Net.Dot;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZTP.Interfaces
+{
+    interface IParser
+    {
+        public DotGraph<int> Run(string path);
+    }
+}
