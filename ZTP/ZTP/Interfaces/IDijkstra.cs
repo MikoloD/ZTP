6d30dfd614198ee1634c39667bcd282fb22af690
MikoloD <grygiel.mikolaj@gmail.com>
2022-03-08 22:40:31 +0100

    Create IDijkstra.cs

---
+++ ZTP/ZTP/Interfaces/IDijkstra.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZTP
+{
+    public interface IDijkstra
+    {
+        public DijkstraResult[] AlghoritmResult { get; set; }
+        public void Run(int[,] adjacencyMatrix, int startVertex);
+    }
+
+}
