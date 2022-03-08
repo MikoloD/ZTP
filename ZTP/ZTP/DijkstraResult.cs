01ff42839b6da67b9417371700430c61d4c54399
MikoloD <grygiel.mikolaj@gmail.com>
2022-03-08 22:21:13 +0100

    Create DijkstraResult.cs

---
+++ ZTP/ZTP/DijkstraResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZTP
+{
+    public class DijkstraResult
+    {
+        public int SourceNodeId { get; set; }
+        public int TargetNodeId { get; set; }
+        public int Value { get; set; }
+        public List<int> Path { get; set; } = new List<int>();
+    }
+}
