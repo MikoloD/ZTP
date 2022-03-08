2a63cc4c1704142cf8072f560ab556ac479cc501
MikoloD <grygiel.mikolaj@gmail.com>
2022-03-08 22:21:11 +0100

    Create GraphParser.cs

---
+++ ZTP/ZTP/GraphParser.cs
@@ -0,0 +1,26 @@
+﻿using Antlr.Runtime;
+using Graphviz4Net.Dot;
+using Graphviz4Net.Dot.AntlrParser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ZTP
+{
+    public class GraphParser
+    {
+        public DotGraph<int> Run(string path)
+        {
+            using StreamReader streamReader = new StreamReader(path);
+            ANTLRStringStream antlrStream = new ANTLRStringStream(streamReader.ReadToEnd());
+            DotGrammarLexer lexer = new DotGrammarLexer(antlrStream);
+            CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+            DotGrammarParser parser = new DotGrammarParser(tokenStream);
+            IntDotGraphBuilder builder = new IntDotGraphBuilder();
+            parser.Builder = builder;
+            parser.dot();
+            return builder.DotGraph;
+        }
+    }
+}
