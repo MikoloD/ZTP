80a540d6d65c316b05ebaf7ddf6624ec29d9106a
MikoloD <grygiel.mikolaj@gmail.com>
2022-03-08 22:40:34 +0100

    Create IAdjacencyMatrix.cs

---
+++ ZTP/ZTP/Interfaces/IAdjacencyMatrix.cs
@@ -0,0 +1,12 @@
+﻿using Graphviz4Net.Dot;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZTP.Interfaces
+{
+    interface IAdjacencyMatrix
+    {
+        public int[,] CreateAdjMatrix(DotGraph<int> Graph);
+    }
+}
