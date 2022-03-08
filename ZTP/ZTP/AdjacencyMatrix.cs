1b6dfc53b428df3d8e58383fdc20115d69a71094
MikoloD <grygiel.mikolaj@gmail.com>
2022-03-08 22:21:19 +0100

    Create AdjacencyMatrix.cs

---
+++ ZTP/ZTP/AdjacencyMatrix.cs
@@ -0,0 +1,25 @@
+﻿using Graphviz4Net.Dot;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ZTP
+{
+    public class AdjacencyMatrix
+    {
+        public int[,] CreateAdjMatrix(DotGraph<int> Graph)
+        {
+            int size = Graph.Vertices.Count();
+            int[,] adjMatrix = new int[size,size];
+            foreach (var item in Graph.VerticesEdges)
+            {
+                int x = item.Source.Id;
+                int y = item.Destination.Id;
+                adjMatrix[x, y] = (int)item.Weight;
+                adjMatrix[y, x] = (int)item.Weight;
+            }
+            return adjMatrix;
+        }
+    }
+}
