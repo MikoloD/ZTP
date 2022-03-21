22ade856e66dcc0394abd67579b8942900d0b133
MikoloD <grygiel.mikolaj@gmail.com>
2022-03-21 12:03:54 +0100

    Create TSP.cs

---
+++ ZTP/ZTP/TSP.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ZTP.Interfaces;
+
+namespace ZTP
+{
+    class TSP : ITSP
+    {
+        private readonly IDijkstra _dijkstra;
+        public TSP(IDijkstra dijkstra)
+        {
+            _dijkstra = dijkstra;
+        }
+
+        public Path Run()
+        {
+            Path path = new Path
+            {
+                Value = 0,
+                Nodes = new int[dijkstraResults.Length]
+            };
+            while(dijkstraResults.Length > 0)
+            {
+                Path result = FindNearest(dijkstraResults);
+                path.Value += result.Value;
+
+            }
+        }
+    }
+}
