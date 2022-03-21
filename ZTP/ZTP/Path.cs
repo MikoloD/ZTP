196667bf42dc13aa1a420aca9d02211e0bbd59dd
MikoloD <grygiel.mikolaj@gmail.com>
2022-03-21 12:03:48 +0100

    Create Path.cs

---
+++ ZTP/ZTP/Path.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZTP
+{
+    class Path
+    {
+        public int[] Nodes { get; set; }
+        public int Value { get; set; }
+    }
+}
