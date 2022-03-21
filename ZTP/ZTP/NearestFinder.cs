75f837f0cbe7c899630e9d1aacabdf04f4850d26
MikoloD <grygiel.mikolaj@gmail.com>
2022-03-21 12:03:44 +0100

    Create NearestFinder.cs

---
+++ ZTP/ZTP/NearestFinder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZTP
+{
+    class NearestFinder
+    {
+        public Path Run(DijkstraResult[] dijkstraResults)
+        {
+            Path edge = new Path
+            {
+                Value = int.MaxValue,
+                Nodes = new int[2]
+            };
+            for (int i = 0; i < dijkstraResults.Length; i++)
+            {
+                if (edge.Value > dijkstraResults[i].Value)
+                {
+                    edge.Value = dijkstraResults[i].Value;
+                    edge.Nodes[0] = dijkstraResults[i].SourceNodeId;
+                    edge.Nodes[1] = dijkstraResults[i].TargetNodeId;
+                }
+            }
+            return edge;
+        }
+    }
+}
