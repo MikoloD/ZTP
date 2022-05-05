aec33f3c89f73269418a87e5e06823b6dd0f3d29
MikoloD <grygiel.mikolaj@gmail.com>
2022-05-05 20:11:40 +0200

    Create INearestFinder.cs

---
+++ ZTP/ZTP/Interfaces/INearestFinder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZTP.Interfaces
+{
+    interface INearestFinder
+    {
+        public Path Run(DijkstraResult[] dijkstraResults);
+    }
+}
