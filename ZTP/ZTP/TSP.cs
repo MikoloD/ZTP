5af671b529089f64326265728810a9105fd84556
MikoloD <grygiel.mikolaj@gmail.com>
2022-05-05 20:12:04 +0200

    Update TSP.cs

--- ZTP/ZTP/TSP.cs
+++ ZTP/ZTP/TSP.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Graphviz4Net.Dot;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using ZTP.Interfaces;
 
@@ -7,10 +10,40 @@ namespace ZTP
 {
     class TSP : ITSP
     {
-        private readonly IDijkstra _dijkstra;
-        public TSP(IDijkstra dijkstra)
+        private readonly ServiceProvider _serviceProvider;
+        public List<int> AddedNodes { get; set; } = new List<int>();
+        public TSP(ServiceProvider serviceProvider)
         {
-            _dijkstra = dijkstra;
+            _serviceProvider = serviceProvider;
+
+        }
+        public Path Run(int StartNode,DotGraph<int> Graph)
+        {
+            IAdjacencyMatrix AdjacencyMatrix = _serviceProvider.GetService<IAdjacencyMatrix>();
+            IDijkstra Dijkstra = _serviceProvider.GetService<IDijkstra>();
+            INearestFinder NearestFinder = _serviceProvider.GetService<INearestFinder>();
+
+            int index = 0;
+            Path result = new Path
+            {
+                Value = 0,
+                Nodes = new int[Graph.Vertices.Count() + 1]
+            };
+            result.Nodes[index] = StartNode;
+
+            int[,] weightMatrix = AdjacencyMatrix.CreateAdjMatrix(Graph);
+            while (AddedNodes.Count() < Graph.Vertices.Count())
+            {
+                Dijkstra.Run(weightMatrix, StartNode);
+                var dijstraResult = Dijkstra.AlghoritmResult.Where(x => AddedNodes.All(y => y != x.TargetNodeId)).ToArray();
+                Path path = NearestFinder.Run(dijstraResult);
+                index++;
+                result.Nodes[index] = path.Nodes[1];
+                result.Value += path.Value;
+                StartNode = path.Nodes[1];
+                AddedNodes.Add(path.Nodes[0]);
+            }
+            return result;
         }
 
         //public Path Run()
