78ed68bc581e4336934dcaefd8cbce5b468c7e92
MikoloD <grygiel.mikolaj@gmail.com>
2022-05-05 20:11:25 +0200

    Create DIBuilder.cs

---
+++ ZTP/ZTP/DIBuilder.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ZTP.Interfaces;
+
+namespace ZTP
+{
+    public class DIBuilder
+    {
+        public ServiceProvider CoreServices { get; set; }
+        public ServiceProvider BuildServices { get; set; }
+        public DIBuilder()
+        {
+            BuildServices = new ServiceCollection()
+                .AddSingleton<IAdjacencyMatrix, AdjacencyMatrix>()
+                .AddSingleton<IDijkstra, Dijkstra>()
+                .AddSingleton<INearestFinder, NearestFinder>()
+                .BuildServiceProvider();
+
+            CoreServices = new ServiceCollection()
+                .AddSingleton<IParser, GraphParser>()
+                .AddSingleton<ITSP, TSP>(config => new TSP(BuildServices))
+                .BuildServiceProvider();
+        }
+    }
+}
