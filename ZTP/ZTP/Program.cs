23d5999569e541d03643dadec1262297a79b5dc9
MikoloD <grygiel.mikolaj@gmail.com>
2022-05-23 21:38:59 +0200

    Update Program.cs

--- ZTP/ZTP/Program.cs
+++ ZTP/ZTP/Program.cs
@@ -1,12 +1,29 @@
 ﻿using Graphviz4Net.Dot;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Diagnostics;
 using ZTP.Interfaces;
 
 namespace ZTP
 {
     internal class Program
     {
+        static void Test(ITSP TSP,int startNode, DotGraph<int> Graf)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            Path result = TSP.Run(startNode, Graf);
+            stopwatch.Stop();
+            PrintSolution(result, stopwatch.Elapsed);
+        }
+        static void PrintSolution(Path result,TimeSpan time)
+        {
+            foreach (var item in result.Nodes)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Wartość: " + result.Value);
+            Console.WriteLine("Czas: " + time);
+        }
         static void Main(string[] args)
         {
             string path = @"Data\graphDuzy.dt";
@@ -18,13 +35,14 @@ namespace ZTP
             DIBuilder DIProvider  = new DIBuilder();
             var serviceProvider = DIProvider.BuildServices;
             DotGraph<int> Graf = serviceProvider.GetService<IParser>().Run(path);
-            ITSP TravelingSalesmanProblem = serviceProvider.GetService<ITSP>();
-            Path result = TravelingSalesmanProblem.Run(startNode,Graf);
-            foreach (var item in result.Nodes)
-            {
-                Console.WriteLine(item);
-            }
-            Console.WriteLine("Wartość: "+ result.Value);
+
+            ITSP NaiveTSP = serviceProvider.GetService<ITSP>();
+            IParallelTSP ParallelNaiveTSP = serviceProvider.GetService<IParallelTSP>();
+
+
+            Test(NaiveTSP, startNode, Graf);
+            //Work in progress
+            //Test(ParallelNaiveTSP, startNode, Graf);
         }
     }
 }
