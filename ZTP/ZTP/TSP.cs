76f9615d0b50e54318e406337e35167b4fb6aada
MikoloD <grygiel.mikolaj@gmail.com>
2022-05-29 23:01:07 +0200

    Update TSP.cs

--- ZTP/ZTP/TSP.cs
+++ ZTP/ZTP/TSP.cs
@@ -22,15 +22,15 @@ namespace ZTP
             _nearestFinder = nearestFinder;
 
         }
-        public Path Run(int StartNode,DotGraph<int> Graph)
+
+        IPath ITSP.Run(int StartNode, DotGraph<int> Graph)
         {
-            int graphSize = Graph.Vertices.Count();
+            int graphSize = Graph.Vertices.Count()-1;
             Path result = new Path
             {
                 Value = 0,
-                Nodes = new int[graphSize + 1]
             };
-            result.Nodes[0] = StartNode;
+            ((List<int>)result.Nodes).Add(StartNode);
 
             int[,] weightMatrix = _adjacencyMatrix.CreateAdjMatrix(Graph);
             for (int i = 0; i < graphSize; i++)
@@ -38,14 +38,17 @@ namespace ZTP
                 _dijkstra.Run(weightMatrix, StartNode);
                 var dijstraResult = _dijkstra.AlghoritmResult.Where(x => AddedNodes.All(y => y != x.TargetNodeId)).ToArray();
                 Path path = _nearestFinder.Run(dijstraResult);
-                result.Nodes[i+1] = path.Nodes[1];
+
+                int sorceNode = path.Nodes.FirstOrDefault();
+                int targetNode = path.Nodes.LastOrDefault();
+                ((List<int>)result.Nodes).Add(targetNode);
                 result.Value += path.Value;
-                StartNode = path.Nodes[1];
-                AddedNodes.Add(path.Nodes[0]);
-            }        
+                StartNode = targetNode;
+                AddedNodes.Add(sorceNode);
+            }
             _dijkstra.Run(weightMatrix, StartNode);
-            result.Nodes[graphSize] = result.Nodes[0];
-            result.Value += _dijkstra.AlghoritmResult[result.Nodes[0]].Value;
+            ((List<int>)result.Nodes).Add(result.Nodes.FirstOrDefault());
+            result.Value += _dijkstra.AlghoritmResult[result.Nodes.FirstOrDefault()].Value;
             return result;
         }
 
