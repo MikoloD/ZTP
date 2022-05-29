1bab0b2aeef849ca2d19c0208018c92ae0d1890d
MikoloD <grygiel.mikolaj@gmail.com>
2022-05-29 23:01:32 +0200

    Create IParallelNearestFinder.cs

---
+++ ZTP/ZTP/Interfaces/IParallelNearestFinder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZTP.Interfaces
+{
+    public interface IParallelNearestFinder
+    {
+        public ParallelSafePath Run(DijkstraResult[] dijkstraResults);
+    }
+}
