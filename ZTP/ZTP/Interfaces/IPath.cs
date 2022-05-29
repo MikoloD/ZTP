3e51d1036593a044dfe03174278095219aeba5d1
MikoloD <grygiel.mikolaj@gmail.com>
2022-05-29 23:01:27 +0200

    Create IPath.cs

---
+++ ZTP/ZTP/Interfaces/IPath.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZTP.Interfaces
+{
+    public  interface IPath
+    {
+        public IEnumerable<int> Nodes { get; set; }
+        public long Value { get; set; }
+    }
+}
