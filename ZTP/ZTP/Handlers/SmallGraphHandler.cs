12bfeef08264e5546f40ebdfa016f0dc421045fa
MikoloD <grygiel.mikolaj@gmail.com>
2022-05-30 20:42:49 +0200

    Create SmallGraphHandler.cs

---
+++ ZTP/ZTP/Handlers/SmallGraphHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZTP.Handlers
+{
+    class SmallGraphHandler : AbstractHandler
+    {
+        public override object Handle(object request)
+        {
+            if ((request as string) == "Banana")
+            {
+                return $"Monkey: I'll eat the {request.ToString()}.\n";
+            }
+            else
+            {
+                return base.Handle(request);
+            }
+        }
+    }
+}
