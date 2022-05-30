7a70e32df22417b7724d41437c97d26d47b4d2c2
MikoloD <grygiel.mikolaj@gmail.com>
2022-05-30 20:42:51 +0200

    Create AbstractHandler.cs

---
+++ ZTP/ZTP/Handlers/AbstractHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ZTP.Interfaces;
+
+namespace ZTP.Handlers
+{
+    abstract class AbstractHandler : IHandler
+    {
+        private IHandler _nextHandler;
+
+        public IHandler SetNext(IHandler handler)
+        {
+            this._nextHandler = handler;
+
+            return handler;
+        }
+
+        public virtual object Handle(object request)
+        {
+            if (this._nextHandler != null)
+            {
+                return this._nextHandler.Handle(request);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
