eadec7d11a50860989432cf58e8fee59edf3fd3b
MikoloD <grygiel.mikolaj@gmail.com>
2022-05-30 22:34:34 +0200

    Update IHandler.cs

--- ZTP/ZTP/Handlers/Interfaces/IHandler.cs
+++ ZTP/ZTP/Handlers/Interfaces/IHandler.cs
@@ -7,6 +7,6 @@ namespace ZTP.Handlers.Interfaces
     public interface IHandler
     {
         IHandler SetNext(IHandler handler);
-        object Handle(object request);
+        object Handle(GraphRequest request);
     }
 }
