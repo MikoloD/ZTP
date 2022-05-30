93df09bb23f5bb180a20aaa13543ec3f79097eee
MikoloD <grygiel.mikolaj@gmail.com>
2022-05-30 20:42:47 +0200

    Create IHandler.cs

---
+++ ZTP/ZTP/Interfaces/IHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZTP.Interfaces
+{
+    public interface IHandler
+    {
+        IHandler SetNext(IHandler handler);
+        object Handle(object request);
+    }
+}
