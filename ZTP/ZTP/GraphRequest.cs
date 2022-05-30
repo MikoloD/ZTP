808763a854cdfe0c4bb842b52dfeabc6f0af428d
MikoloD <grygiel.mikolaj@gmail.com>
2022-05-30 22:34:42 +0200

    Create GraphRequest.cs

---
+++ ZTP/ZTP/GraphRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ZTP.Interfaces;
+using ZTP.TSPAlghoritms.Interfaces;
+
+namespace ZTP
+{
+    public class GraphRequest
+    {
+        public int StartingNode { get; set; }
+        public string Path { get; set; }
+        public GraphRequest()
+        {
+
+        }
+        public GraphRequest(int startingNode, string path)
+        {
+            StartingNode = startingNode;
+            Path = path;
+        }
+    }
+}
